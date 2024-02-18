8acb6e21e9683135daf66027605c1cda3647efda
Kapardhi <118623227+kapardhi03@users.noreply.github.com>
2024-02-18 14:32:51 +0530

    Update get_sumary.py

--- get_sumary.py
+++ get_sumary.py
@@ -5,7 +5,7 @@ def generate_summary(full_text , max_token_limit,language):
     if language != "English": max_token_limit = int(max_token_limit * 3) 
     prompt = f"give me a comnbined summary of these articles in {language}:\n"+full_text
 
-    client = OpenAI(api_key = "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")
+    client = OpenAI(api_key = "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")
 
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
