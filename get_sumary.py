fcdd452ad7c6cdbdf668336af626626e8416fafc
Kapardhi <118623227+kapardhi03@users.noreply.github.com>
2024-05-14 11:43:47 +0530

    Update get_sumary.py

--- get_sumary.py
+++ get_sumary.py
@@ -1,20 +1,26 @@
-from openai import OpenAI
+from openai import OpenAI, APIError
 
+def generate_summary(full_text, max_token_limit, language):
+    if language != "English":
+        max_token_limit = int(max_token_limit * 3)
 
-def generate_summary(full_text , max_token_limit,language):
-    if language != "English": max_token_limit = int(max_token_limit * 3) 
-    prompt = f"give me a comnbined summary of these articles in {language}:\n"+full_text
+    prompt = f"give me a combined summary of these articles in {language}:\n" + full_text
 
-    client = OpenAI(api_key = "sk-proj-HE0HHifzC3W7XTg7D1SOT3BlbkFJ74NFhtMa8PEMA7UUnCSD")
+    try:
+        client = OpenAI(api_key="sk-proj-HE0HHifzC3W7XTg7D1SOT3BlbkFJ74NFhtMa8PEMA7UUnCSD")
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=max_token_limit
+        )
 
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt}
-    ],max_tokens= max_token_limit
-    )
+        return completion.choices[0].message.content
 
-    # print(completion.choices[0].message.content)
-    return completion.choices[0].message.content
-
+    except APIError as e:
+        if e.code == "invalid_api_key":
+            raise Exception("API limit expired or not supported by OpenAI")
+        else:
+            raise e
